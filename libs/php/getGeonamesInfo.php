f8573e81aefcf271f296135904e359e09fc108bf
Joshua-Barry-Baker <josh.barry.baker@googlemail.com>
2020-12-02 13:26:54 +0000

    name change and complete php

--- libs/php/getGeonamesInfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $eta=-hrtime(true);
-    
-    $url='http://api.geonames.org/countryInfoJSON?formatted=true&lang='. $_REQUEST['lang'] . '&country' . $_REQUEST['country'] . '$username=JBBaker&style=full';
-    
-    $eta+=hrtime(true);
-    $output['status']['returnedIn']=$eta/1e+6 . 'ms';
-?>
