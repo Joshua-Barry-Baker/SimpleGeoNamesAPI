10e6b6e4c6a0cc9adefc2f2540d49c79610cc96b
Joshua-Barry-Baker <josh.barry.baker@googlemail.com>
2020-12-08 10:13:28 +0000

    Table now only shows on successful fetch and error report to user on error

--- libs/js/CountryParse.js
+++ libs/js/CountryParse.js
@@ -1,7 +1,8 @@
 $('#btnRun').click(function(){
 
     $.ajax({
-        url: `libs/php/${selFetch}`,
+        
+        url: "libs/php/getCountryInfo.php",
         type: 'POST',
         dataType: 'json',
         data: {
@@ -9,11 +10,28 @@ $('#btnRun').click(function(){
             lang: $('#selLanguage').val()
         },
         success: function(result) {
-
             console.log(result);
             
             if (result.status.name == "ok") {
-                
+
+                document.getElementById("apiTableDisplay").innerHTML =
+                `<div>
+                    <tr>
+                        <th>Continent</th>
+                        <th>Capital</th>
+                        <th>Languages</th>
+                        <th>Population</th>
+                        <th>Area (km<sup>2</sup>)</th>
+                    </tr>
+                    <tr>
+                        <td id="txtContinent"></td>
+                        <td id="txtCapital"></td>
+                        <td id="txtLanguages"></td>
+                        <td id="txtPopulation"></td>
+                        <td id="txtArea"></td>
+                    </tr>
+                </div>`;
+
                 $('#txtContinent').html(result['data'][0]['continent']);
                 $('#txtCapital').html(result['data'][0]['capital']);
                 $('#txtLanguages').html(result['data'][0]['languages']);
@@ -24,6 +42,7 @@ $('#btnRun').click(function(){
         },
         error: function(jqXHR, textStatus, errorThrown){
             console.log({jqXHR, textStatus, errorThrown})
+            document.getElementById("apiTableDisplay").innerHTML = `Error ${jqXHR.status}: ${errorThrown}`;
         }
     });
 });
